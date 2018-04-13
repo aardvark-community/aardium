bc756c87ba3628f5f7d5d03d9b8656bad1be375c
krauthaufen <krauthaufen@awx.at>
2018-04-13 15:00:40 +0200

    sync

--- preload.js
+++ preload.js
@@ -1,4 +1,4 @@
-const {dialog} = require('electron').remote
+const {dialog, Menu} = require('electron').remote
 
 var aardvark = {};
 document.aardvark = aardvark;
@@ -6,4 +6,9 @@ document.aardvark = aardvark;
 aardvark.openFileDialog = function(config, callback) {
 	if(!callback) callback = config;
 	dialog.showOpenDialog({properties: ['openFile', 'multiSelections']}, callback);
+};
+
+aardvark.setMenu = function(template) {
+	const menu = Menu.buildFromTemplate(template)
+	Menu.setApplicationMenu(menu)
 };
