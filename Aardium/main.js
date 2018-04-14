b9242388a3c293dbe7a735ca2887627a47ee9aea
krauthaufen <krauthaufen@awx.at>
2018-04-14 12:33:32 +0200

    added some options to Aardium

--- Aardium/main.js
+++ Aardium/main.js
@@ -6,32 +6,100 @@ const BrowserWindow = electron.BrowserWindow
 
 const path = require('path')
 const url = require('url')
+const getopt = require('node-getopt')
 
+const options =
+[
+  ['w' , 'width=ARG'              , 'initial window width'],
+  ['h' , 'height=ARG'             , 'initial window height'],
+  ['u' , 'url=ARG'                , 'initial url' ],
+  ['g' , 'debug'                  , 'show debug tools'],
+  ['i' , 'icon=ARG'               , 'icon file'],
+  ['t' , 'title=ARG'              , 'window title'],
+  ['m' , 'menu'                   , 'display default menu'],
+  [''  , 'fullscreen'             , 'display fullscreen window']
+];
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
+
+
 function createWindow () {
+
+
+  var argv = process.argv;
+  if(!argv) argv = [];
+
+  var res = getopt.create(options).bindHelp().parse(argv); 
+  var preventTitleChange = true;
+  var opt = res.options;
+  if(!opt.width) opt.width = 1024;
+  if(!opt.height) opt.height = 768;
+  if(!opt.url) opt.url = "http://ask.aardvark.graphics";
+  if(!opt.icon) opt.icon = path.join(__dirname, 'aardvark.ico');
+  if(!opt.title) {
+    opt.title = "Aardvark rocks \\o/";
+    preventTitleChange = false;
+  }
+
   // Create the browser window.
   mainWindow = 
 	new BrowserWindow({ 
-		width: 800,
-		height: 600,
-		title: "Aardvark rocks \\o/",
-		icon: path.join(__dirname, 'aardvark.ico'),
+		width: parseInt(opt.width),
+		height: parseInt(opt.height),
+		title: opt.title,
+    icon: opt.icon,
+    fullscreen: opt.fullscreen,
+    fullscreenable: true,
 		webPreferences: { 
 			nodeIntegration: false, 
-			webSecurity: false, 
+      webSecurity: false, 
+      devTools: true,
 			preload: path.join(__dirname, 'preload.js')
 		}
-	});
-  mainWindow.setMenu(null);
+  });
 
-  var url = "http://localhost:4321/";
-  if(process.argv.length > 2) url = process.argv[2];
-	
+  electron.app.on('browser-window-created',function(e,window) {
+      window.setMenu(null);
+      window.setTitle(opt.title);
+      window.on('page-title-updated', (e,c) => {
+        e.preventDefault();
+      });
+  });
+
+
+  if(!opt.menu) mainWindow.setMenu(null);
+  // if(process.argv.length > 2) url = process.argv[2];
+  if(preventTitleChange) {
+    mainWindow.on('page-title-updated', (e,c) => {
+      e.preventDefault();
+    });
+  }
+
+  electron.globalShortcut.register('F11',() => {
+    var n = !mainWindow.isFullScreen();
+    console.log("fullscreen: " + n);
+    mainWindow.setFullScreen(n);
+  });
+  if(opt.debug) {
+    
+    electron.globalShortcut.register('F10',() => {
+      console.log("devtools");
+      mainWindow.webContents.toggleDevTools();
+    });
+
+    electron.globalShortcut.register('F5',() => {
+      console.log("reload");
+      mainWindow.webContents.reload(true);
+    });
+
+
+
+  }
+
   // and load the index.html of the app.
-  mainWindow.loadURL(url);
+  mainWindow.loadURL(opt.url);
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
