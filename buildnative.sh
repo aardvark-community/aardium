64e5a09b3a85bee525963f7f47f53df6cbd6e0c8
krauthaufen <krauthaufen@awx.at>
2022-01-18 23:52:59 +0100

    more automated builds

--- buildnative.sh
+++ buildnative.sh
@@ -1,4 +1,26 @@
+#!/bin/sh
+
 cd Aardium
 npm install -g yarn
 yarn install
+
+os=$(uname -s)
+if [ $os == "Darwin" ];
+then
+    arch=$1;
+    if [ "$arch" == "" ];
+    then
+        arch=$(uname -m)
+    fi
+
+    if [ $arch == "x86_64" ];
+    then
+        yarn dist:darwin:x64
+    else
+        yarn dist:darwin:arm64
+    fi
+
+else
+    yarn dist:linux
+fi
 yarn dist
