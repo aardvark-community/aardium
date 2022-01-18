898d27202207e9df4d904a4bbb42d45466138f58
krauthaufen <krauthaufen@awx.at>
2022-01-18 19:01:32 +0100

    build test

---
+++ buildnative.sh
@@ -0,0 +1,22 @@
+cd Aardium
+
+os=$(uname -s)
+arch=$1;
+if [ "$arch" == "" ];
+then
+    arch=$(uname -m)
+fi
+
+cd Aardium
+
+if [ $os == "Darwin" ];
+then
+    if [ $arch == "x86_64" ];
+    then
+        yarn dist:darwin:x64
+    else
+        yarn dist:darwin:arm64
+    fi
+else
+        yarn dist:linux
+fi
